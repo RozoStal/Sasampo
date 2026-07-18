900895b3bb304d35d535ae6f6afbd65608dd5aea
Alexandr <osetrovadd@gmail.com>
2026-07-18 03:08:56 +0300

    fix(NotificationManager.cpp): pointer handling has been optimized.

--- Managers/src/NotificationManager.cpp
+++ Managers/src/NotificationManager.cpp
@@ -8,8 +8,8 @@ void NotificationManager::addSender(std::shared_ptr<INotificationSender> sender)
 
 void NotificationManager::sendToAll(const Notification& notification) {
     int nSendedMessages = 0;
-    for (auto sender : m_senders){
-        if (sender.get()->send(notification)) nSendedMessages++; 
+    for (auto& sender : m_senders){
+        if (sender->send(notification)) nSendedMessages++; 
     }
     std::cout << "Sasampo::NotificationManager: " <<  nSendedMessages << " sent, " << m_senders.size() - nSendedMessages << " aborted" << std::endl;
 }
